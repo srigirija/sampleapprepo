7a328bd88ed45912332f082a80ce55994806174b
srigirija <mareddi.srigirija0110@gmail.com>
2021-09-20 10:50:40 +0530

    stage show method added

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -5,6 +5,9 @@ public class BootClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello from STS!");
+		show();
 	}
-
+	public static void show() {
+		System.out.println("show method here!");
+	}
 }
