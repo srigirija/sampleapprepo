52d1cdf94f93ef99bcb4e80a7f83ae8918fee82c
srigirija <mareddi.srigirija0110@gmail.com>
2021-09-20 18:42:11 +0530

    yearly report added

---
+++ sampleapp/src/main/java/org/cap/demo/YearlyReport.java
@@ -0,0 +1,13 @@
+package org.cap.demo;
+
+public class YearlyReport {
+	public void calculate() {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
